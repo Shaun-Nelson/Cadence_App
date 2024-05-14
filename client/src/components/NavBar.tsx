0dad96bfbc5f10ba8d5c674055b371c808c602ba
Shaun Nelson <shaunptnelson@gmail.com>
2024-05-14 15:38:20 -0400

    Refactored controller functions

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { useLogoutMutation } from "../slices/usersApiSlice";
 import { logout } from "../slices/authSlice";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "../store";
+
+// Components
+import NavItem from "./NavItem";
 
 const NavBar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -34,31 +36,21 @@ const NavBar = () => {
   return (
     <nav>
       <ul className='navbar'>
-        <li className='nav-item'>
-          <Link to='/'>Home</Link>
-        </li>
-        <li className='nav-item'>
-          <Link to='/signup'>Sign Up</Link>
-        </li>
+        <NavItem linkTo='/' bodyText='Home' />
+        <NavItem linkTo='/signup' bodyText='Sign-Up' />
         {/* If user is logged in, display Logout, else display Login */}
         {isLoggedIn ? (
           <>
-            <li className='nav-item'>
-              <Link to='/playlists'>My Playlists</Link>
-            </li>
-            <li className='nav-item'>
-              <Link to='/profile'>User Profile</Link>
-            </li>
-            <li className='nav-item'>
-              <Link onClick={handleLogout} to='/logout'>
-                Logout
-              </Link>
-            </li>
+            <NavItem linkTo='/playlists' bodyText='My Playlists' />
+            <NavItem linkTo='/profile' bodyText='User Profile' />
+            <NavItem
+              linkTo='/logout'
+              bodyText='Logout'
+              onClickHandler={handleLogout}
+            />
           </>
         ) : (
-          <li className='nav-item'>
-            <Link to='/login'>Login</Link>
-          </li>
+          <NavItem linkTo='/login' bodyText='Login' />
         )}
       </ul>
     </nav>
