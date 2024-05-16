5b65fb54ccfad01636619530aa271c49a26a50e9
Shaun Nelson <shaunptnelson@gmail.com>
2024-05-16 03:47:56 -0400

    H

--- server/server.ts
+++ server/server.ts
@@ -6,6 +6,7 @@ const cors = require("cors");
 const routes = require("./routes");
 require("dotenv").config();
 
+//Config
 const PORT = process.env.PORT || 3001;
 const app = express();
 const sess = {
