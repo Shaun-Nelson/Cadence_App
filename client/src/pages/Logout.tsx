384343c8e0545bf5d9323185da476105cd429a9d
Shaun Nelson <shaunptnelson@gmail.com>
2024-05-18 14:36:27 -0400

    refactored frontend and backend

--- client/src/pages/Logout.tsx
+++ client/src/pages/Logout.tsx
@@ -1,33 +1,33 @@
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-import { toast } from "react-toastify";
+// import { useNavigate } from "react-router-dom";
+// import { useEffect } from "react";
+// import { toast } from "react-toastify";
 
 const Logout = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    const logout = async () => {
-      try {
-        const response = await fetch(
-          `${import.meta.env.VITE_API_URL}/api/logout`,
-          {
-            credentials: "include",
-          }
-        );
+  // useEffect(() => {
+  //   const logout = async () => {
+  //     try {
+  //       const response = await fetch(
+  //         `${import.meta.env.VITE_API_URL}/api/logout`,
+  //         {
+  //           credentials: "include",
+  //         }
+  //       );
 
-        if (response.ok) {
-          navigate("/");
-        } else {
-          console.error("Failed to log out");
-          toast.error("Failed to log out");
-        }
-      } catch (error) {
-        console.error(error);
-        toast.error("Failed to log out");
-      }
-    };
-    logout();
-  }, [navigate]);
+  //       if (response.ok) {
+  //         navigate("/");
+  //       } else {
+  //         console.error("Failed to log out");
+  //         toast.error("Failed to log out");
+  //       }
+  //     } catch (error) {
+  //       console.error(error);
+  //       toast.error("Failed to log out");
+  //     }
+  //   };
+  //   logout();
+  // }, [navigate]);
 
   return <div>Logging out...</div>;
 };
